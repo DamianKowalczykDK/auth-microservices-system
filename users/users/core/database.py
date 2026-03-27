0fc079deccfe1b6b3bd5e203fcf30710b3051aab
Damian <damiankowalczyk3688@gmail.com>
2026-03-27 20:53:57 +0100

    feat(user): add MongoDB initialization with Beanie

---
+++ users/users/core/database.py
@@ -0,0 +1,9 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from beanie import init_beanie
+from users.core.config import settings
+from users.domain.models import User
+
+
+async def init_db() -> None:
+    client: AsyncIOMotorClient = AsyncIOMotorClient(settings.MONGODB_URI)
+    await init_beanie(database=client[settings.MONGODB_DB], document_models=[User])  # type: ignore
