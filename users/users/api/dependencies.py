4bdfd6fff6546a3f1fe487326c832478c75eb3df
Damian <damiankowalczyk3688@gmail.com>
2026-03-28 21:47:43 +0100

    feat(users): add dependency injection for user repository and email service

---
+++ users/users/api/dependencies.py
@@ -0,0 +1,26 @@
+from users.respositories.user_repository import UserRepository
+from users.services.email_service import EmailService
+from users.core.email import email_settings
+from functools import lru_cache
+from fastapi_mail import FastMail
+from typing import Annotated
+from fastapi import Depends
+
+
+@lru_cache()
+def get_user_repository() -> UserRepository:
+    return UserRepository()
+
+UserRepoDep = Annotated[UserRepository, Depends(get_user_repository)]
+
+@lru_cache()
+def get_fastmail() -> FastMail:
+    return FastMail(email_settings)
+
+FastMailDep = Annotated[FastMail, Depends(get_fastmail)]
+
+@lru_cache()
+def get_email_service(mailer: FastMailDep) -> EmailService:
+    return EmailService(mailer)
+
+EmailServiceDep = Annotated[EmailService, Depends(get_email_service)]
