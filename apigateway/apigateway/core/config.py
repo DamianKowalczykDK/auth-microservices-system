232ee369dacf407cfbcc9cedc0ff84a78e6aee9e
Damian <damiankowalczyk3688@gmail.com>
2026-04-02 21:06:59 +0200

    feat(apigateway): add settings configuration

---
+++ apigateway/apigateway/core/config.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    DEBUG: bool = False
+
+    USERS_SERVICE_URL: str
+    HTTP_TIMEOUT: float
+
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
+
+settings = Settings()
