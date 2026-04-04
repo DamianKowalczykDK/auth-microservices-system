b7ba77e0a36c1161e9bd1423e6ac6c0c0689ab58
Damian <damiankowalczyk3688@gmail.com>
2026-04-04 17:41:29 +0200

    feat(apigateway): add schemas

--- apigateway/apigateway/domain/schemas.py
+++ apigateway/apigateway/domain/schemas.py
@@ -1,21 +1,42 @@
-from pydantic import BaseModel, EmailStr
+from typing import Annotated, Literal
+from pydantic import BaseModel, EmailStr, Field
 
+Password = Annotated[str, Field(min_length=6)]
+Username = Annotated[str, Field(min_length=3, max_length=64, description="Unique username")]
+Identifier = Annotated[str, Field(min_length=3, max_length=64, description="Username or email")]
+
+class UserCreate(BaseModel):
+    username: Username
+    email: EmailStr
+    password: Password
+    password_confirmation: Password
+    role: Literal["admin", "user"] = "user"
 
 class UserLogin(BaseModel):
     identifier: str
     password: str
 
+class UserResetPassword(BaseModel):
+    token : str
+    new_password: Password
+
 class UserRead(BaseModel):
     id: str
     username: str
     email: EmailStr
-    role: str
+    role: Literal["admin", "user"] = "user"
     is_active: bool
     mfa_secret: str | None = None
 
 class TokenPair(BaseModel):
     access_token: str
     refresh_token: str
+
+class MfaSetup(BaseModel):
+    user_id: str
+    provisioning_uri: str
+    qr_code_base64: str
+
 
 class MfaRequired(BaseModel):
     mfa_required: bool = True
