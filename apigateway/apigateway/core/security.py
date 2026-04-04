098fe0c7cf51c299b09cdf52f214dcbbb6e815e3
Damian <damiankowalczyk3688@gmail.com>
2026-04-04 17:30:01 +0200

    fix(apigateway): add # type: ignore to suppress mypy warning

--- apigateway/apigateway/core/security.py
+++ apigateway/apigateway/core/security.py
@@ -1,9 +1,15 @@
 from datetime import datetime, timedelta, timezone
 from apigateway.core.config import settings
-from jose import jwt
+from jose import jwt # type: ignore
 
 def create_access_token(subject: str) -> str:
-    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode = {"sub": str(subject), "exp": expire}
+    utc_now = datetime.now(timezone.utc)
+    expire = utc_now + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode = {
+        "sub": str(subject),
+        "type": "access",
+        "exp": expire,
+        "iat": utc_now
+    }
     return jwt.encode(to_encode, settings.JWT_SECRET_KEY, settings.JWT_ALGORITHM)
 
